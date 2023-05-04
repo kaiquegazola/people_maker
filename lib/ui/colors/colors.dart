424f9e9fe914afa8f3cf9f6d4eae66943e98bacb
Kaique Gazola <me@kaique.dev>
2023-05-04 15:38:09 -0300

    chore: add project colors

---
+++ lib/ui/colors/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+abstract class PeopleColors {
+  static const Color neutral050 = Color(0xffF4F5F7);
+  static const Color neutral100 = Color(0xffE5E7EB);
+  static const Color neutral200 = Color(0xffD1D5DB);
+  static const Color neutral300 = Color(0xff9CA3AF);
+  static const Color neutral400 = Color(0xff6B7280);
+  static const Color neutral500 = Color(0xff4B5563);
+  static const Color neutral600 = Color(0xff374151);
+  static const Color neutral700 = Color(0xff252F3F);
+  static const Color neutral800 = Color(0xff161E2E);
+  static const Color neutral900 = Color(0xff0A1723);
+
+  static const Color blue050 = Color(0xffEBF5FF);
+  static const Color blue100 = Color(0xffD0E1FF);
+  static const Color blue200 = Color(0xffA5C8FF);
+  static const Color blue300 = Color(0xff7AAFED);
+  static const Color blue400 = Color(0xff5397E8);
+  static const Color blue500 = Color(0xff1A56DB);
+  static const Color blue600 = Color(0xff1548C6);
+  static const Color blue700 = Color(0xff103AA2);
+  static const Color blue800 = Color(0xff0C2D7D);
+  static const Color blue900 = Color(0xff082057);
+}
