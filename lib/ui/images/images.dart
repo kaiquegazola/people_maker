cf6604124a6ee7b25e7cebee4024e53fb3fe8e40
Kaique Gazola <me@kaique.dev>
2023-05-04 15:38:45 -0300

    chore: add project images

---
+++ lib/ui/images/images.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+enum Images {
+  background('wicked_background.svg');
+
+  const Images(this.fileName);
+
+  final String fileName;
+
+  Widget image({
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+  }) {
+    return SvgPicture.asset(
+      'assets/images/$fileName',
+      width: width,
+      height: height,
+      fit: fit,
+    );
+  }
+}
